eb240251790b808efa700377165e56b29169bbd8
Ahmed Gado <ahmed_gado2001@yahoo.com>
2021-08-03 16:55:47 +0200

    make structure

---
+++ AGMVVM/Scenes/Home/Model/HomeModel.swift
@@ -0,0 +1,32 @@
+//
+//  HomeModel.swift
+//  AGMVVM
+//
+//  Created by ahmed gado on 03/08/2021.
+//  Copyright © 2021 ahmed gado. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - HomeModel
+struct HomeModel: Codable {
+    let status: String
+    let data: [HomeItem]
+    let message: String
+}
+
+// MARK: - Datum
+struct HomeItem: Codable {
+    let id: Int
+    let employeeName: String
+    let employeeSalary, employeeAge: Int
+    let profileImage: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case employeeName = "employee_name"
+        case employeeSalary = "employee_salary"
+        case employeeAge = "employee_age"
+        case profileImage = "profile_image"
+    }
+}
---
+++ AGMVVM/Scenes/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  AGMVVM
+//
+//  Created by ahmed gado on 03/08/2021.
+//  Copyright © 2021 ahmed gado. All rights reserved.
+//
+
+import Foundation
